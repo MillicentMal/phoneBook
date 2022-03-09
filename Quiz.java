1be385bc11dadd1cd80460ede91da136e63b5a30
Millicent Malinga <71253520+MillicentMal@users.noreply.github.com>
2022-03-09 17:24:06 +0200

    Working properly quiz solution

--- Quiz.java
+++ Quiz.java
@@ -7,48 +7,54 @@ import java.util.concurrent.TimeUnit;
 
 public class Quiz {
     public static void quiz_runner (String questions[], String answers[][], int correct_answers[])  throws InputMismatchException {
-        String response = "y";
+        System.out.println("Welcome to the general knowledge quiz.");
         boolean keepPlaying = true;
+        int i = 0;
         while (keepPlaying == true) {
-            for (int i = 0; i < questions.length; i++) {
-                System.out.println(questions[i]);
-                for (int j =0; j < 5; j++){
-                    System.out.println(answers[i][j]);
-                }
-                long startTime = System.nanoTime();
+            if (i == questions.length) {
+                i = 0;
+            }
+            System.out.println(questions[i]);
+
+            for (int j =0; j < 5; j++){
+            System.out.println(answers[i][j]);
+            }
+            long startTime = System.nanoTime();
    // ... the code being measured ...
    
-                Scanner user_answer = new Scanner(System.in); //System.in is a standard input stream  
-                System.out.print("Do you want to keep playing? ");  
-                int ans = user_answer.nextInt();
-                long estimatedTime = System.nanoTime() - startTime;
-                long timetaken = TimeUnit.SECONDS.convert(estimatedTime, TimeUnit.NANOSECONDS);
+            Scanner user_answer = new Scanner(System.in); //System.in is a standard input stream  
+            System.out.print("What is your answer?");  
+            int ans = user_answer.nextInt();
+            long estimatedTime = System.nanoTime() - startTime;
+            long timetaken = TimeUnit.SECONDS.convert(estimatedTime, TimeUnit.NANOSECONDS);
 
-                if (ans == correct_answers[i]) {
-                    System.out.println("Well Done. That is correct");
-                }
-                else if (ans != correct_answers[i]){
-                    System.out.println("Wrng answer. Keep playing");
-                }
-                else {
-    
-                }
-                System.out.println("You took " + timetaken);
-
-                Scanner user_response = new Scanner(System.in); //System.in is a standard input stream  
-                System.out.print("Do you want to keep playing? ");  
-                String str1= user_response.nextLine();  
-                if (str1.toLowerCase() == response){
+            if (ans == correct_answers[i]) {
+                System.out.println("Well Done. That is correct");
+            }
+            else if (ans != correct_answers[i]){
+                System.out.println("Wrng answer. Sorry!");
+            }
+            System.out.println("You took " + timetaken + " seconds to answer.");
+            Scanner user_response = new Scanner(System.in);
+                System.out.print("Do you want to keep playing? Answer 1 for yes and 0 for no. ");  
+                int str1= user_response.nextInt();  
+                if (str1 == 1){
                     keepPlaying = true;
+                    if ( i == questions.length - 1){
+                    i = 0;
+                    }
+                    else {
+                        i++;
+                    }
                 } 
-                else if (str1.toLowerCase() != response){
-                    keepPlaying = false;
-                    System.out.println("Thank you for playing");     
-                          }
+                else {
+                    System.out.println("Thank you for playing, see you!");   
+                    break; 
+                            }
+            }
             }
 
-        }
-    }
+
     public static void main( String[] args) {
         String questions [] = {
             "Who is the president of Rwanda?", "Who founded ALU?"
